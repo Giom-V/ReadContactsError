93a1ea2cf369ba568c9196d44f1cfd29997f657b
giom <guillaume@vernade.fr>
2014-09-25 10:41:46 +0200

    Initial commit

---
+++ app/src/main/java/readcontactserror/slyslayer/com/readcontactserror/MyActivity.java
@@ -0,0 +1,58 @@
+package readcontactserror.slyslayer.com.readcontactserror;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.app.SearchableInfo;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+        setupSearchView();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        final TextView resultText = (TextView) findViewById(R.id.searchViewResult);
+        if (ContactsContract.Intents.SEARCH_SUGGESTION_CLICKED.equals(intent.getAction())) {
+            //handles suggestion clicked query
+            String displayName = getDisplayNameForContact(intent);
+            resultText.setText(displayName);
+        } else if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            // handles a search query
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            resultText.setText("should search for query: '" + query + "'...");
+        }
+    }
+
+    private void setupSearchView() {
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        final SearchView searchView = (SearchView) findViewById(R.id.searchView);
+        SearchableInfo searchableInfo = searchManager.getSearchableInfo(getComponentName());
+        searchView.setSearchableInfo(searchableInfo);
+        searchView.setFocusable(true);
+        searchView.setIconified(false);
+        searchView.requestFocusFromTouch();
+    }
+
+    private String getDisplayNameForContact(Intent intent) {
+        Cursor phoneCursor = getContentResolver().query(intent.getData(), null, null, null, null);
+        phoneCursor.moveToFirst();
+        int idDisplayName = phoneCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+        String name = phoneCursor.getString(idDisplayName);
+        phoneCursor.close();
+        return name;
+    }
+}
